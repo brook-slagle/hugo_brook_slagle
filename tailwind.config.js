ce677555dce34865fe4344bd51a393dd76ac84ce
Brook Slagle <brook.slagle@trojans.dsu.edu>
2022-09-19 14:24:20 -0500

    postcss + tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  content: [
+    "./content/**/*.md",
+    "./layouts/**/*.html",
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: [
+    require('@tailwindcss/typography'),
+  ]
+}
